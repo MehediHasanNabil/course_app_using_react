cc7ddf16e790ad18ab544587f3cd7cc7636d89de
MehediHasanNabil <63330618+MehediHasanNabil@users.noreply.github.com>
2020-09-22 16:54:14 +0600

    Add files via upload

---
+++ precache-manifest.2bef3152e185309075a5e8684e86eb87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f788bbcdbc609157b02bc30f2316a3",
+    "url": "/course_app_using_react/index.html"
+  },
+  {
+    "revision": "0f91a6c584e4b2e5995c",
+    "url": "/course_app_using_react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "617bf5aea305917dfc8d",
+    "url": "/course_app_using_react/static/css/main.ba2d0a52.chunk.css"
+  },
+  {
+    "revision": "0f91a6c584e4b2e5995c",
+    "url": "/course_app_using_react/static/js/2.82bcc6ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/course_app_using_react/static/js/2.82bcc6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617bf5aea305917dfc8d",
+    "url": "/course_app_using_react/static/js/main.d42be4be.chunk.js"
+  },
+  {
+    "revision": "4a8b7e4cf451681a3d97",
+    "url": "/course_app_using_react/static/js/runtime-main.7f488c66.js"
+  }
+]);
